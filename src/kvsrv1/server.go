f441cd1dea8de9d78aab64f187af29da88e2c319
Michael Muz <20muzm01@gmail.com>
2026-05-17 19:35:16 -0400

    start the kv server

--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -2,6 +2,9 @@ package kvsrv
 
 import (
 	"log"
+	"net"
+	"net/http"
+	netrpc "net/rpc"
 	"sync"
 
 	"6.5840/kvsrv1/rpc"
@@ -11,24 +14,16 @@ import (
 
 const Debug = false
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
-	if Debug {
-		log.Printf(format, a...)
-	}
-	return
+type value struct {
+	val     string
+	version rpc.Tversion
 }
-
 
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
-}
-
-func MakeKVServer() *KVServer {
-	kv := &KVServer{}
-	// Your code here.
-	return kv
+	kv map[string]value
 }
 
 // Get returns the value and version for args.Key, if args.Key
@@ -42,10 +37,43 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // If the key doesn't exist, Put installs the value if the
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	e, ok := kv.kv[args.Key]
+	if ok {
+		if args.Version == e.version {
+			kv.kv[args.Key] = value{args.Value, args.Version + 1}
+			reply.Err = rpc.OK
+		} else {
+			reply.Err = rpc.ErrVersion
+		}
+	} else {
+		if args.Version == 0 {
+			kv.kv[args.Key] = value{args.Value, 0}
+			reply.Err = rpc.OK
+		} else {
+			reply.Err = rpc.ErrNoKey
+		}
+	}
 }
 
-
+func MakeKVServer() *KVServer {
+	kv := &KVServer{} // no need to worry ab this being stack alloc with dangling pointer bc go knows unlike C
+	if err := netrpc.Register(kv); err != nil {
+		log.Fatal("Couldn't register:", err)
+	}
+	netrpc.HandleHTTP()
+	l, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Fatal("Listen error:", err)
+	}
+	go func() {
+		if err := http.Serve(l, nil); err != nil {
+			log.Fatal("Serve error: ", err)
+		}
+	}()
+	return kv
+}
 
 // You can ignore all arguments; they are for replicated KVservers
 func StartKVServer(tc *tester.TesterClnt, ends []*labrpc.ClientEnd, gid tester.Tgid, srv int, persister *tester.Persister) []any {
